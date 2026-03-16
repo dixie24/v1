04979c0cb88d86eb966bd575d93a25c715994be3
dixie24 <rpamg255@gmail.com>
2026-03-16 23:34:32 +0600

    v1.3

--- v1/src/Main.java
+++ v1/src/Main.java
@@ -1,7 +1,21 @@
-String name = "Алибек";
-int age = 25;
-double price = 9.99;
-boolean isActive = true;
+public class Main {
+    public static void main(String[] args) {
+        String name = "Алибек";
+        int age = 25;
 
-void main() {
+        System.out.println("Привет, " + name + "!");
+        System.out.println("Тебе " + age + " лет");
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static int multiply(int a, int b, int c) {
+        return a * b * c;
+    }
 }
