6fb2911072b31b23b7238dbb38472723b91810d8
dixie24 <rpamg255@gmail.com>
2026-03-18 23:50:55 +0600

    v1.4

--- v1/src/Main.java
+++ v1/src/Main.java
@@ -1,37 +1,5 @@
 public class Main {
-
-    public static void main(String[] args) {
-        // вызываем методы
-        sayHello();
-
-        int result = add(5, 3);
-        System.out.println(result); // 8
-
-        String message = greet("Алибек");
-        System.out.println(message); // Hello, Алибек!
-    }
-
     public static void sayHello() {
         System.out.println("Привет!");
-    }
-
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static String greet(String name) {
-        return "Hello, " + name + "!";
-    }
-
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static double calculateTax(double price, double rate) {
-        return price * rate / 100;
-    }
-
-    public static void sayHello() {
-        System.out.println("Привет!23");
     }
 }
