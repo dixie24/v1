d224d36a69e3a17f26d40fb495f802c296403b03
dixie24 <rpamg255@gmail.com>
2026-03-18 23:46:19 +0600

    v1.3

--- v1/src/Main.java
+++ v1/src/Main.java
@@ -1,7 +1,18 @@
+
 String name = "Алибек";
 int age = 25;
 double price = 9.99;
 boolean isActive = true;
+public static int multiply(int a, int b) {
+    return a * b;
+}
 
+public static double multiply(double a, double b) {
+    return a * b;
+}
+
+public static int multiply(int a, int b, int c) {
+    return a * b * c;
+}
 void main() {
 }
