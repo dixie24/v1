afa906a83b08b962b2b85825666fad65a04811f5
dixie24 <rpamg255@gmail.com>
2026-03-18 23:46:19 +0600

    v1.3

--- v1/src/Main.java
+++ v1/src/Main.java
@@ -1,18 +1,25 @@
+public class Main {
 
-String name = "Алибек";
-int age = 25;
-double price = 9.99;
-boolean isActive = true;
-public static int multiply(int a, int b) {
-    return a * b;
-}
+    public static void main(String[] args) {
+        // вызываем методы
+        sayHello();
 
-public static double multiply(double a, double b) {
-    return a * b;
-}
+        int result = add(5, 3);
+        System.out.println(result); // 8
 
-public static int multiply(int a, int b, int c) {
-    return a * b * c;
-}
-void main() {
+        String message = greet("Алибек");
+        System.out.println(message); // Hello, Алибек!
+    }
+
+    public static void sayHello() {
+        System.out.println("Привет!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
 }
