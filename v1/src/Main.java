4563ce25371feb5321dc8bf5f598ab325cb83962
dixie24 <rpamg255@gmail.com>
2026-04-09 23:57:49 +0600

    4.2

--- v1/src/Main.java
+++ v1/src/Main.java
@@ -16,7 +16,7 @@ public class Main {
     }
 
     public static int add(int a, int b) {
-        return a * b + b;
+        return a + a * b + b;
     }
 
     public static String greet(String name) {
