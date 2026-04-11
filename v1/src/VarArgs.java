d7a77ef3f65a6dca2fcd4b63ea20302f793defac
dixie24 <rpamg255@gmail.com>
2026-04-11 23:11:34 +0600

    v4.5

--- v1/src/VarArgs.java
+++ v1/src/VarArgs.java
@@ -1,16 +1,33 @@
+//public class VarArgs {
+//    static void vaTest( int ... v) {
+//    System.out.print("Koличecтвo аргументов : " + v.length + " Содержимое : ");
+//    for (int x : v)
+//        System.out.print(x + " ");
+//    System.out.println();
+//}
+//    public static void main(String[] args)
+//    {
+//// Обратите внимание , что теперь метод vaTest ()
+//// можно вызывать с переменным числом аргументов.
+//        vaTest ( 1,0 ) ; // 1 аргумент
+//        vaTest (1, 2, 3 ) ; // 3 аргумента
+//        vaTest (); // без аргументов {
+//}
+//}
+
 public class VarArgs {
-static void vaTest( int ... v) {
-    System.out.print("Koличecтвo аргументов : " + v.length + " Содержимое : ");
-    for (int x : v)
-        System.out.print(x + " ");
-    System.out.println();
-}
+    static void vaTest(String msg, int ... v) {
+        System.out.print(msg + v.length + " Содержимое : ");
+        for (int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
     public static void main(String[] args)
     {
 // Обратите внимание , что теперь метод vaTest ()
 // можно вызывать с переменным числом аргументов.
-        vaTest ( 1,0 ) ; // 1 аргумент
+        vaTest (10 ) ; // 1 аргумент
         vaTest (1, 2, 3 ) ; // 3 аргумента
         vaTest (); // без аргументов {
-}
+    }
 }
