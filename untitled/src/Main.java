37c037f870499190b31e2e6c0d36830dda7958e3
dixie24 <rpamg255@gmail.com>
2026-06-06 23:58:50 +0600

    upd_2

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
-public class Main{
-    @Override
-    public int hashCode() {
-        return super.hashCode();
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1st figure: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter 2nd figure: ");
+        int num2 = scanner.nextInt();
+
+        int res;
+
+        System.out.println("Action: ");
+        String action = scanner.nextLine()
     }
+
 }
