b96235b0fc6b70bae2eea6a77e2c185f5c40861a
dixie24 <rpamg255@gmail.com>
2026-07-05 23:44:13 +0600

    v4.7

---
+++ v1/src/Product.java
@@ -0,0 +1,10 @@
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+}{
+}
---
+++ v1/src/ProductController.java
@@ -0,0 +1,18 @@
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository repository;
+
+    @GetMapping
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return repository.save(product);
+    }
+}{
+}
