3c2cf7e04792bf6650c24ed49c2bfd40fdcb0f87
dixie24 <rpamg255@gmail.com>
2026-07-09 23:55:47 +0600

    upd_7.7

--- untitled/src/Login.java
+++ untitled/src/Login.java
@@ -1,2 +1,17 @@
 public class Login {
+
+    private String validUsername = "admin";
+    private String validPassword = "password123";
+
+    /**
+     * Метод для проверки данных пользователя
+     *
+     * @param username введенное имя
+     * @param password введенный пароль
+     * @return true, если данные верны, иначе false
+     */
+    public boolean authenticate(String username, String password) {
+        return validUsername.equals(username) && validPassword.equals(password);
+
+    }
 }
