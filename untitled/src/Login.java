ed7854b859c0049f0eb63e7a97959acf10b2ea39
dixie24 <rpamg255@gmail.com>
2026-07-09 23:56:25 +0600

    upd_7.9

--- untitled/src/Login.java
+++ untitled/src/Login.java
@@ -14,4 +14,17 @@ public class Login {
         return validUsername.equals(username) && validPassword.equals(password);
 
     }
+    public static void main(String[] args) {
+        LoginSystem login = new LoginSystem();
+
+        // Пример использования
+        String inputUser = "admin";
+        String inputPass = "password123";
+
+        if (login.authenticate(inputUser, inputPass)) {
+            System.out.println("Успешный вход!");
+        } else {
+            System.out.println("Ошибка: неверное имя пользователя или пароль.");
+        }
+    }
 }
