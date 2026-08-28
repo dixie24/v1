1712bf2721aa54997f305a0d929093ab182d84e5
dixie24 <rpamg255@gmail.com>
2026-08-28 23:58:55 +0600

    upd_8.59

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int name = sc.nextInt();
-        System.out.println(name);
+        System.out.println("ytyт!");
     }
 }
